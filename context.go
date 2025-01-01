0596db29923abdbf4d4fab62a58c69514a50f16c
x6nux <xunl47236@gmail.com>
2025-01-02 02:37:51 +0800

    feat(context): 添加设置和添加请求头的方法
    - 新增 AddHeader 方法，用于在 Context 中添加单个请求头
    - 新增 AddHeaders 方法，用于批量添加请求头
    - 新增 SetHeaders 方法，用于设置多个请求头

--- context.go
+++ context.go
@@ -53,6 +53,26 @@ func (c *Context) SetHeader(key string, value any) {
 	c.Header[key] = value
 }
 
+func (c *Context) AddHeader(key string, value any) {
+	if _, ok := c.Header[key]; !ok {
+		c.Header[key] = value
+	}
+
+}
+
+func (c *Context) AddHeaders(h header) {
+	for k, v := range h {
+		if _, ok := c.Header[k]; !ok {
+			c.Header[k] = v
+		}
+	}
+}
+
+func (c *Context) SetHeaders(h map[string]any) {
+	for k, v := range h {
+		c.Header[k] = v
+	}
+}
 func (c *Context) DeleteHeader(key string) {
 	delete(c.Header, key)
 }
